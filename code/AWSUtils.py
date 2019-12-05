dd33c98ce88cca32272c78a8362d939c5f48908b
Zoltan Nemeth <zoltan@nprovision.com>
2019-12-05 13:35:21 -0500

    Updated AWSUtils.py to include support for listing bucket contents

--- code/AWSUtils.py
+++ code/AWSUtils.py
@@ -4,7 +4,7 @@ import sys
 import boto3
 
 
-def create_s3_bucket(session, newBucketName):
+def s3_create_bucket(session, newBucketName):
 
 	# Create an S3 resource 
 	s3 = session.resource('s3')
@@ -17,9 +17,19 @@ def create_s3_bucket(session, newBucketName):
 		print(bucket)
 
 
+def s3_list_objects_in_bucket(session, bucketname):
+
+	# Create an S3 resource 
+	s3 = session.resource('s3')
+
+	# List objects (files) in an s3 bucket
+	for obj in s3.Bucket(bucketname).objects.all():
+		print(obj)
+
+
 def main():
 
-	print(f'AWS Python Utility Tools v1.0')
+	print(f'AWS Python Utility Tools v1.0.1')
 
 	args = len(sys.argv) - 1
 	if args == 0:
@@ -28,14 +38,17 @@ def main():
 
 
 	# Determine function type
-	
+
 
 	# Create session
 	session = boto3.Session(profile_name='nprovision')
 
 
  	# Create bucket
-	create_s3_bucket(session, sys.argv[1])
+	#s3_create_bucket(session, sys.argv[1])
+
+	# List a bucket's contents
+	s3_list_objects_in_bucket(session, sys.argv[1])
 
 
 if __name__ == '__main__':
